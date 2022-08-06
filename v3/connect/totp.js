37ec810aceeb67e3894a71245e7288c5b6915b0e
Joe Ertaba <belaviyo@gmail.com>
2022-08-06 01:07:00 -0400

    new fixes for TOTP based on #62

--- v3/connect/totp.js
+++ v3/connect/totp.js
@@ -1,35 +1,30 @@
 class TOTP {
   static hexToBuf(hex) {
-    const r = new Uint8Array(hex.length / 2);
-    hex.match(/[\dA-F]{2}/gi).forEach((c, n) => {
-      r[n] = parseInt(c, 16);
-    });
-
-    return r;
+    const a = hex.match(/[\dA-F]{2}/gi).map(c => parseInt(c, 16));
+    return new Uint8Array(a);
   }
   static b32ToHex(base32) {
     const base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
     let bits = '';
     let hex = '';
-    let i = 0;
-    while (i < base32.length) {
+    for (let i = 0; i < base32.length; i++) {
       const val = base32chars.indexOf(base32.charAt(i).toUpperCase());
       bits += val.toString(2).padStart(5, '0');
-      i++;
     }
-    i = 0;
-    while (i + 4 <= bits.length) {
-      const chunk = bits.substr(i, 4);
-      hex = hex + parseInt(chunk, 2).toString(16);
-      i += 4;
+    for (let i = 0; i + 8 <= bits.length; i += 8) {
+      const chunks = [bits.substr(i, 4), bits.substr(i + 4, 4)];
+      for (const chunk of chunks) {
+        hex += parseInt(chunk, 2).toString(16);
+      }
     }
+
     return hex;
   }
   static decToHex(s) {
-    return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
+    return Math.round(s).toString(16);
   }
   static counter(period = 30) {
-    const epoch = Math.round(new Date().getTime() / 1000.0);
+    const epoch = Math.round(Date.now() / 1000);
     return TOTP.decToHex(Math.floor(epoch / period)).padStart(16, '0');
   }
   async generate(secret, period = 30, digits = 6) {
